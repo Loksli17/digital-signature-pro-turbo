3b323d4fbde85f1a835fa85573cfd55ca2e332bc
RobinLoskli <ami0504@mail.ru>
2021-03-17 20:53:51 +1000

    fileupload progress

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QFileDialog>
 #include <QImage>
 #include <QString>
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -22,9 +24,19 @@ void MainWindow::on_loadImage_clicked()
     ui->imageWrap->clear();
 
     this->imagePath = QFileDialog::getOpenFileName(nullptr, "Choose image", "C:/desktop", "*.jpg");
+    QFile file(this->imagePath);
+
+
+    QStringList lst      = this->imagePath.split('/');
+    QString     fileName = lst[lst.count() - 1];
 
     if(this->imagePath != ""){
         this->image.load(this->imagePath);
         ui->imageWrap->setPixmap(this->image);
+        QMessageBox::
+        QMessageBox::information(this, "Success", "File: " + fileName + " was opened");
+    }else{
+        QMessageBox::warning(this, "Error", "File wasn't opened");
+        return;
     }
 }
